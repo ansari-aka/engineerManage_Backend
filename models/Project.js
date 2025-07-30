94aed7fc3ad2afcef789adb2c4acd0a0c03aac29
Admin2git <aka62218@gmail.com>
2025-07-31 00:27:13 +0530

    fix : project  models

--- models/Project.js
+++ models/Project.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Engineer = require('./Engineer');
 
 const projectSchema = new mongoose.Schema({
   projectName: {
@@ -22,6 +23,17 @@ const projectSchema = new mongoose.Schema({
     type: Number, // Total hours required for the project
     required: true,
   },
+});
+
+projectSchema.pre("findOneAndDelete", async function (next) {
+  const project = await this.model.findOne(this.getQuery());
+  if (project) {
+    const res = await Engineer.deleteMany({ assignedProjects: project._id });
+    console.log(
+      `🗑️ Deleted ${res.deletedCount} tasks that belonged to deleted project: ${project._id}`
+    );
+  }
+  next();
 });
 
 const Project = mongoose.model('Project', projectSchema);
